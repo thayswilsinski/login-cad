0b71e19693b84488439eef3f846d9e34257394c1
Thais <thays.wilsinski@alunoescolassociais.org.br>
2024-08-30 11:26:51 -0300

    corrigindo o erro e finalizando o projeto

--- controller.js
+++ controller.js
@@ -1,14 +1,12 @@
-//Função para validar o acesso na tela de login
+// Função para validar o acesso na tela de login
 function acessar() {
-    // Obtém os valores dos campos de email e senha
     let loginEmail = document.getElementById("loginEmail").value;
     let loginSenha = document.getElementById("loginSenha").value;
-    let logincpf = document.getElementById("loginCpf").value; // Adicionei a variável logincpf
  
-    if (!loginEmail || !loginSenha || !logincpf) {
+    if (!loginEmail || !loginSenha) {
         alert("Favor preencher todos os campos");
     } else {
-        window.location.href = "cadastro.html";
+        window.location.href = "cadastro.html"; // Redireciona para a página de cadastro
     }
 }
  
@@ -19,7 +17,6 @@ var cpfLista = [];
  
 // Função para salvar o nome, email e CPF do usuário na lista
 function salvarUser() {
-    // Obtém os valores dos campos de nome de usuário, email e CPF
     let nomeUser = document.getElementById("nomeUser").value;
     let emailUser = document.getElementById("emailUser").value;
     let cpfUser = document.getElementById("cpfUser").value;
@@ -39,28 +36,23 @@ function salvarUser() {
  
 // Função para criar e atualizar a lista de usuários na tela
 function criarLista() {
-    // Cria o cabeçalho da tabela
     let tabela = "<tr><th>Nome Usuário</th><th>Email</th><th>CPF</th><th>Ações</th></tr>";
  
-    // Itera sobre todos os nomes, emails e CPFs na lista
     for (let i = 0; i < dadosLista.length; i++) {
         tabela += "<tr><td>" + dadosLista[i] + "</td><td>" + EmailLista[i] + "</td><td>" + cpfLista[i] + "</td>" +
-            "<td><button type='button' onclick='editar(" + i + ")' class='btn-rosa'>Editar</button>" +
-            "<button type='button' onclick='excluir(" + i + ")' class='btn-rosa'>Excluir</button></td></tr>";
+            "<td><button type='button' onclick='editar(" + i + ")' class='btn-roxo'>Editar</button>" +
+            "<button type='button' onclick='excluir(" + i + ")' class='btn-roxo'>Excluir</button></td></tr>";
     }
  
-    // Atualiza o conteúdo da tabela com a nova lista
     document.getElementById("tabela").innerHTML = tabela;
 }
  
 // Função para editar um nome, email e CPF na lista
 function editar(i) {
-    // Preenche os campos com o nome, email e CPF selecionados para edição
     document.getElementById('nomeUser').value = dadosLista[i];
     document.getElementById('emailUser').value = EmailLista[i];
     document.getElementById('cpfUser').value = cpfLista[i];
  
-    // Remove o item da lista para evitar duplicação
     dadosLista.splice(i, 1);
     EmailLista.splice(i, 1);
     cpfLista.splice(i, 1);
@@ -68,47 +60,25 @@ function editar(i) {
  
 // Função para excluir um nome, email e CPF da lista
 function excluir(i) {
-    // Remove o item da lista de dados, email e CPF
     dadosLista.splice(i, 1);
     EmailLista.splice(i, 1);
     cpfLista.splice(i, 1);
  
-    // Atualiza a lista exibida na tela
     criarLista();
 }
  
-// Função para validar o email
-function validarEmail() {
-    let email = document.forms[0].email.value;
- 
-    // Verifica se o campo de email está vazio ou não contém '@' ou '.'
-    if (email === "" || email.indexOf('@') === -1 || email.indexOf('.') === -1) {
-        // Se alguma das condições acima for verdadeira, exibe um alerta
-        alert("Por favor, informe um e-mail válido");
-        return false;
-    } else {
-        alert("Email informado com sucesso");
-        document.getElementById('email').innerHTML = email;
-        return true;
-    }
-}
-//###############################################################################################################
- 
- 
- 
 // Função para validar o CPF
 function validarCPF(cpf) {
-    cpf = cpf.replace(/[^\d]+/g, ''); // Remove caracteres não numéricos do CPF
+    cpf = cpf.replace(/[^\d]+/g, '');
  
     if (cpf.length !== 11 || /^(\d)\1{10}$/.test(cpf)) {
-        alert("CPF inválido"); // Adiciona alerta se o CPF não for válido
+        alert("CPF inválido. Utilize o formato XXX.XXX.XXX-XX");
         return false;
     }
  
     let soma = 0;
     let resto;
  
-    // Validação do 1º dígito verificador (10º dígito)
     for (let i = 1; i <= 9; i++) {
         soma += parseInt(cpf.substring(i - 1, i)) * (11 - i);
     }
@@ -120,11 +90,10 @@ function validarCPF(cpf) {
     }
  
     if (resto !== parseInt(cpf.substring(9, 10))) {
-        alert("CPF inválido"); // Adiciona alerta se o CPF não for válido
+        alert("CPF inválido");
         return false;
     }
  
-    // Validação do 2º dígito verificador (11º dígito)
     soma = 0;
     for (let i = 1; i <= 10; i++) {
         soma += parseInt(cpf.substring(i - 1, i)) * (12 - i);
@@ -137,9 +106,9 @@ function validarCPF(cpf) {
     }
  
     if (resto !== parseInt(cpf.substring(10, 11))) {
-        alert("CPF inválido"); // Adiciona alerta se o CPF não for válido
+        alert("CPF inválido");
         return false;
     }
  
-    return true; // Retorna true se o CPF é válido
+    return true;
 }
